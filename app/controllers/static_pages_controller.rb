a7116f753fb0993b0b3d2032eb71455be03ab33c
datbk206 <dat10a9dqh@gmail.com>
2021-08-12 08:23:49 +0000

    Chapter 14: Following user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    return unless logged_in
+    return unless logged_in?
 
     @micropost = current_user.microposts.build
     @feed_items = current_user.feed.page params[:page]
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,16 +6,19 @@ class Micropost < ApplicationRecord
   POST_ATTRS = %i(content image).freeze
 
   validates :user_id, presence: true
-  validates :content, presence: true, length: {maximum: Settings.content.max_size}
+  validates :content, presence: true, length: {
+    maximum: Settings.content.max_size
+  }
   validates :image, content_type: {
     in: Settings.micropost.img_type,
-    message: t("micropost.invalid_format")
+    message: I18n.t("micropost.invalid_format")
   },
   size: {
     less_than: Settings.micropost.size.megabytes,
-    message: t("micropost.require_size")
+    message: I18n.t("micropost.require_size")
   }
   scope :newest, ->{order(created_at: :desc)}
+  scope :by_user_id, ->(ids){where user_id: ids}
 
   def display_image
     image.variant resize: Settings.micropost.image_size
